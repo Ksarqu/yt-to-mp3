b665961b83e7f971e23549039e02c16b8d17f916
Ksarqu <ksarqu@gmail.com>
2021-12-12 16:04:36 +0100

    intial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# yt-to-mp3
+
+yt-to-mp3 is a music downloader from youtube to mp3 format,
+<br>
+**based on yt-dlp, it means no problems with download speed :)**
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install needed packages first.
+
+```bash
+pip install -r requirements.txt
+```
+btw, i am using **python 3.9.7**, if you are using older or something, please notice you may have problems with it!
+
+
+## Usage
+
+```bash
+python main.py
+```
+
+first of all script creates **_config.json_** file.
+<br>
+you can change default settings like **path** or **language [EN/PL]**
+<br>
+
+## Contributing
+
+Pull requests are welcome. If you have any problems, please open an issue first, I can do something for it, thanks!
+
---
+++ main.py
@@ -0,0 +1,71 @@
+import json
+import os
+import pathlib
+import re
+import shutil
+from os import system, rename, path
+
+import yt_dlp as youtube_dl
+
+lang_files_dir = "languages"
+lang_files = os.listdir(lang_files_dir)
+languages_data = {}
+for file in lang_files:
+    language = file.split(".")[0]  # separates the filename from the extension
+    with open(f"{lang_files_dir}\\{file}", "r", encoding="utf-8") as current_file:
+        languages_data[language] = json.load(current_file)
+
+urls = []
+
+if not path.isfile("config.json"):
+    with open("config.json", 'w', encoding="utf-8") as config_file:
+        config_data = {"path": "C:\\Users\\XXXUSERXXX\\Desktop",
+                       "language": "EN"}
+        json.dump(config_data,
+                  config_file,
+                  indent=4,
+                  sort_keys=True)
+        print("Please edit the config file as you want!")
+        exit()
+else:
+    with open("config.json", "r") as config_file:
+        config_data = json.load(config_file)
+        lang = config_data["language"]
+
+
+def collect_urls():
+    while True:
+        print(languages_data[lang]["period_warner"])
+        url = input(languages_data[lang]["enter_url"])
+        system("cls")
+        if url == ".":
+            break
+        else:
+            pass
+        if "youtube.com" in url:
+            urls.append(url)
+        else:
+            print(languages_data[lang]["wrong_url"])
+
+
+def download():
+    for url in urls:
+        video_info = youtube_dl.YoutubeDL().extract_info(url=url, download=False)
+        filename = re.sub('[#<$+%>!`&*|{}@:/?=]', '', video_info['title'])[:200] + ".mp3"
+        options = {
+            'format': 'bestaudio/best',
+            'keepvideo': False,
+            'outtmpl': "music.mp3",
+        }
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+        with open("config.json", "r") as f:
+            destination_file = json.load(f)
+        source = str(pathlib.Path(__file__).parent)
+        destination = destination_file["path"] + f"\\{filename}"
+        rename("music.mp3", filename)
+        shutil.move(source + f"\\{filename}", destination)
+
+
+collect_urls()
+download()
